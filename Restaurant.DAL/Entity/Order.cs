7ea590913bfebeaf24895ec36796c3c99c29850b
fatimamb-blip <fatimamb@code.edu.az>
2026-01-24 10:44:09 +0400

    First Commit

---
+++ Restaurant.DAL/Entity/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.DAL.Models
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public List<OrderItem> Items { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
